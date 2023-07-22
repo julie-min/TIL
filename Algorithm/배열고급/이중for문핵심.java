7fda8d62a26c51ecea6d05b0ee0d65c769a8fd77
julie-min <jiyun3664@gmail.com>
2023-07-22 19:21:26 +0900

    commit

---
+++ Algorithm/배열고급/이중for문핵심.java
@@ -0,0 +1,15 @@
+package 배열고급;
+
+public class 이중for문핵심 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] num = new int[3];
+		for (int i = 0; i < num.length; i++) {
+			System.out.println(i + " " +num[i]);
+		}
+		
+		int[][] num2 = new int[2][];
+	}
+
+}
